3b206f33615db3feb309b34cfa4f341823e666c0
Alikarakalla <66522458+Alikarakalla@users.noreply.github.com>
2025-06-04 00:50:57 +0300

    fix navbar load animation

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,9 @@
-$(function() {
-  $("#navbar-placeholder").load("navbar.html");
+$(function () {
+  $("#navbar-placeholder").load("navbar.html", function () {
+    if ($(window).scrollTop() > 300) {
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    } else {
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
+    }
+  });
 });
